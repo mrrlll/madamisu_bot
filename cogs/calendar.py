68c1bd0b9b9c7285b20d88536c6b3b58fe0b1600
charlotte <intamiiiii+1@gmail.com>
2021-09-16 17:43:30 +0900

    カレンダーの一部機能実装

--- cogs/calendar.py
+++ cogs/calendar.py
@@ -7,6 +7,7 @@ import discord
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+import os
 
 
 class calendar(commands.Cog):
@@ -18,13 +19,15 @@ class calendar(commands.Cog):
     # コマンドの例
     @commands.command()
     async def calendar(self, ctx):
+
+        # カレンダー部分のスクリーンショット
         path = "C:/bin/chromedriver.exe"
 
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         driver = webdriver.Chrome(options = options,executable_path = path)
-        driver.maximize_window()
+        driver.set_window_size(1920, 1080)
 
         # ドライバが設定されるまでの待ち時間
         driver.implicitly_wait(10)
@@ -32,14 +35,22 @@ class calendar(commands.Cog):
         driver.get('https://calendar.google.com/calendar/embed?src=epion1516%40gmail.com&ctz=Asia%2FTokyo')
         time.sleep(5)
 
+        # 指定の要素をpngでキャプチャ
         png = driver.find_element_by_class_name('mv-container').screenshot_as_png
 
+        # ファイルの保存
         with open ('./img.png', 'wb') as f:
             f.write(png)
 
+        # カレンダーチャンネルのクリア
+        channel = discord.utils.get(ctx.guild.text_channels, name = "カレンダー")
+        await channel.purge()
+
+        # カレンダーのキャプチャを送信
+        imgpath = os.getcwd() + "/img.png"
+        await channel.send(file = discord.File(imgpath))
+
         driver.close()
-
-
 
 
 # Bot本体側からコグを読み込む際に呼び出される関数
